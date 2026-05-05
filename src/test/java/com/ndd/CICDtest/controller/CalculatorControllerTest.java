fd9b3c683a48a75daa41c100e97bef33e7dbabd5
Minseo Jeon <jmsparrowk@naver.com>
2026-05-05 19:48:28 +0900

    CICD fail

--- src/test/java/com/ndd/CICDtest/controller/CalculatorControllerTest.java
+++ src/test/java/com/ndd/CICDtest/controller/CalculatorControllerTest.java
@@ -9,16 +9,16 @@ class CalculatorControllerTest {
     CalculatorController controller = new CalculatorController();
 
     // ✅ 성공 케이스
-    @Test
-    void divide_success() {
-        int result = controller.divide(10, 2);
-        assertEquals(5, result);
-    }
-
-    // ❌ 실패 케이스 (일부러 틀리게)
 //    @Test
-//    void divide_fail() {
+//    void divide_success() {
 //        int result = controller.divide(10, 2);
-//        assertEquals(3, result); // 일부러 틀림 → CI 실패
+//        assertEquals(5, result);
 //    }
+
+//     ❌ 실패 케이스 (일부러 틀리게)
+    @Test
+    void divide_fail() {
+        int result = controller.divide(10, 2);
+        assertEquals(3, result); // 일부러 틀림 → CI 실패
+    }
 }
